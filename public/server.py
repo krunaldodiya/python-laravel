a2de1f35ba43831eda952ebd51fb3ba774e33e9c
Krunal Dodiya <kunal.dodiya1@gmail.com>
2023-07-17 10:18:59 +0530

    done

--- public/server.py
+++ public/server.py
@@ -7,11 +7,15 @@ class Server:
         self.client_url = self.__parse_url(scope["client"])
         self.server_url = self.__parse_url(scope["server"])
 
-        self.http_host = scope["server"][0]
+        self.server_host = scope["server"][0]
         self.server_port = scope["server"][1]
 
+        self.scheme = scope["scheme"]
         self.query_string = scope["query_string"]
-        self.request_method = scope["method"]
+        self.method = scope["method"]
+        self.root_path = scope["root_path"]
+        self.path = scope["path"]
+        self.raw_path = scope["raw_path"]
 
         self.headers = scope["headers"]
 
