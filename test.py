0c767f226be2298aeccedf4e5587c22026577421
Krunal Dodiya <kunal.dodiya1@gmail.com>
2023-07-21 09:05:30 +0530

    done

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from wsgi import app
 
 
 def run():
-    run_simple(app, hostname="127.0.0.1", port=3000)
+    run_simple(application=app, hostname="127.0.0.1", port=3000)
 
 
 if __name__ == "__main__":
