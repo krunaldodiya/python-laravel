a667abcc194fe93a49ba268e619b396cea08a934
Krunal Dodiya <kunal.dodiya1@gmail.com>
2024-09-25 06:38:05 +0530

    added gate.define and gate.check

--- Illuminate/Routing/RoutingServiceProvider.py
+++ Illuminate/Routing/RoutingServiceProvider.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Type
+from Illuminate.Contracts.Foundation.Application import Application
 from Illuminate.Routing.CallableDispatcher import CallableDispatcher
 from Illuminate.Routing.Contracts.CallableDispatcher import (
     CallableDispatcher as CallableDispatcherContract,
@@ -15,12 +15,9 @@ from Illuminate.Routing.Router import Router
 from Illuminate.Http.Request import Request
 from Illuminate.Http.ResponseFactory import ResponseFactory
 
-if TYPE_CHECKING:
-    from Illuminate.Foundation.Application import Application
-
 
 class RoutingServiceProvider(ServiceProvider):
-    def __init__(self, app: Type["Application"]) -> None:
+    def __init__(self, app: Application) -> None:
         self.__app = app
 
     def register(self):
@@ -37,13 +34,13 @@ class RoutingServiceProvider(ServiceProvider):
         pass
 
     def __register_router(self):
-        def lambda_function(app: Type["Application"]):
+        def lambda_function(app: Application):
             return Router(self.__app, self.__app.make("events"))
 
         self.__app.singleton("router", lambda_function)
 
     def __register_url_generator(self):
-        def lambda_function(app: Type["Application"]):
+        def lambda_function(app: Application):
             router = self.__app.make("router")
             request = self.__app.make("request")
             config = self.__app.make("config")
@@ -57,19 +54,19 @@ class RoutingServiceProvider(ServiceProvider):
         self.__app.singleton("url", lambda_function)
 
     def __register_redirector(self):
-        def lambda_function(app: Type["Application"]):
+        def lambda_function(app: Application):
             return Redirector(self.__app, self.__app.make("url"))
 
         self.__app.singleton("redirect", lambda_function)
 
     def __register_http_request(self):
-        def lambda_function(app: Type["Application"]):
+        def lambda_function(app: Application):
             return Request(self.__app)
 
         self.__app.singleton("request", lambda_function)
 
     def __register_http_response(self):
-        def lambda_function(app: Type["Application"]):
+        def lambda_function(app: Application):
             return ResponseFactory(self.__app)
 
         self.__app.singleton("response", lambda_function)
@@ -78,13 +75,13 @@ class RoutingServiceProvider(ServiceProvider):
         pass
 
     def __register_callable_dispatcher(self):
-        def lambda_function(app: Type["Application"]):
+        def lambda_function(app: Application):
             return CallableDispatcher(self.__app)
 
         self.__app.singleton(CallableDispatcherContract, lambda_function)
 
     def __register_controller_dispatcher(self):
-        def lambda_function(app: Type["Application"]):
+        def lambda_function(app: Application):
             return ControllerDispatcher(self.__app)
 
         self.__app.singleton(ControllerDispatcherContract, lambda_function)
