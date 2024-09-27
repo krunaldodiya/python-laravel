4630bd26b4ce1457de03564e277bc314ecc0472e
Krunal Dodiya <kunal.dodiya1@gmail.com>
2024-09-27 12:23:57 +0530

    improved routing

--- Illuminate/Support/Facades/Gate.py
+++ Illuminate/Support/Facades/Gate.py
@@ -1,8 +1,7 @@
 from Illuminate.Support.Facades.Facade import Facade
-from Illuminate.Contracts.Auth.Access.Gate import Gate as GateContract
 
 
 class Gate(metaclass=Facade):
     @staticmethod
     def get_facade_accessor():
-        return GateContract
+        return "gate"
